35cac7a7ad6d1d5dd4443f11093d82100cbee681
David Pinilla <contact@davidpinilla.com>
2020-09-21 16:30:40 +0200

    added locations and characters routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { LocationListComponent } from './components/location-list/location-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'characters', component: CharacterListComponent },
+  { path: 'locations', component: LocationListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterListComponent } from './components/character-list/character-list.component';
 import { CharacterItemComponent } from './components/character-item/character-item.component';
+import { LocationListComponent } from './components/location-list/location-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CharacterListComponent,
-    CharacterItemComponent
+    CharacterItemComponent,
+    LocationListComponent
   ],
   imports: [
     BrowserModule,
